9a9978f5903d7e76fdaa83968ea4cf57289f4125
Kyle-Atienza <atienzakylefrancis@gmail.com>
2021-07-12 23:10:36 +0800

    Update Code

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-var services = document.querySelector(".services")
-var service_items = document.querySelector(".services>.container>.items").children
-var service_name = document.querySelector(".services>.container>.title>h2")
-console.log(service_name)
+
 
 function service_title(){
+
+    var services = document.querySelector(".services")
+    var service_items = document.querySelector(".services>.container>.items").children
+    var service_name = document.querySelector(".services>.container>.title>h2")
+
     services.addEventListener("mouseout", function(){
         service_name.innerHTML = "WHAT I DO"
     })
@@ -29,3 +31,27 @@ function service_title(){
         
     }
 }
+
+function change_gallery(category){
+
+    var gallery_group = document.querySelector("#works>main>.container>.gallery").children
+    var gallery_tab = document.querySelector("#works>main>.container>.categories").children
+    
+    for (let i = 0; i < gallery_group.length; i++) {
+        gallery_group[i].classList.remove("active")
+        if (gallery_group[i].classList.contains(category)) {
+            gallery_group[i].classList.add("active")
+        } 
+    }
+
+    for (let i = 0; i < gallery_tab.length; i++) {
+        gallery_tab[i].classList.remove("active")
+        if (gallery_tab[i].classList.contains(category)) {
+            gallery_tab[i].classList.add("active")
+        }
+        
+    }
+    
+
+}
+
